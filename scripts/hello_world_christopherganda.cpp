9255e0c3897766c1b59dcecdd816d4e4613f4d1c
christopher.ganda <christopher.ganda@mikroskil.ac.id>
2017-10-30 14:56:44 +0700

    edit contributor, add c++ file

---
+++ scripts/hello_world_christopherganda.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // LANGUAGE: C++
+    // ENV: gnu gc++
+    // AUTHOR: Christopher Ganda
+    // GITHUB: https://github.com/christopherganda
+    cout<<"Hello World!"<<endl;
+}
